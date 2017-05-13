1e27bb600d5d49301912767dd2b76909814a55f0
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-05-13 22:52:13 +0300

    Change branch in deploy.sh to master

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ git config user.name "Zarema Khalilova"
 git config user.email "zarema.khalilova@gmail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/Zmoki/zmoki.github.io.git"
-git fetch upstream && git reset upstream/gh-pages
+git fetch upstream && git reset upstream/master
 
 touch .
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push -q upstream HEAD:master
